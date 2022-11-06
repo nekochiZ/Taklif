c242211a03cda8433dff07b41a3b32dde2bb5fe1
nekochiZ <117333619+nekochiZ@users.noreply.github.com>
2022-11-06 23:18:41 +0330

    Add files via upload

---
+++ code 1.cs
@@ -0,0 +1,8 @@
+using System;
+class test
+{
+    public static void Main(string []Args)
+    {
+        Console.WriteLine("tets{0} ,Args[0]")
+    }
+}
---
+++ code 2.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Booleans
+{
+    public static void main()
+    {
+        bool content = true;
+        bool nocontent = false;
+        Console.WriteLine("test is {0}", content);
+        Console.WriteLine("test is {0}", nocontent);
+    }
+}
